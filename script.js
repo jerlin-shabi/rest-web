f28f75a48afa3debafc92d149709354e460142f7
Jerlin shabi <138958887+jerlin-shabi@users.noreply.github.com>
2023-11-10 10:09:17 +0000

    header fixed

--- script.js
+++ script.js
@@ -2,3 +2,19 @@ function animateFromBottom() {
   var content = document.querySelector('.content');
   content.classList.add('show');
 }
+
+document.addEventListener('scroll', function () {
+  var headerContainer = document.querySelector('header');
+
+  if (window.scrollY > 150) {
+    headerContainer.style.backgroundColor = 'red'; // Change background color on scroll
+    headerContainer.style.opacity = '1';
+  } else {
+    headerContainer.style.backgroundColor = 'rgba(51, 51, 51, 0)'; // Transparent background when at the top
+    headerContainer.style.opacity = '1'; // Fully visible at the top
+  }
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  animateFromBottom();
+});
