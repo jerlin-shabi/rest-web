5fe22979f05e8201dde0ca4d0b7644c0f62bf873
Bennyhinn18 <137780675+Bennyhinn18@users.noreply.github.com>
2023-11-17 12:42:08 +0000

    responsive header

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ document.addEventListener('scroll', function () {
   var headerContainer = document.querySelector('header');
 
   if (window.scrollY > 150) {
-    headerContainer.style.backgroundColor = '#333';
+    headerContainer.style.backgroundColor = 'red';
     headerContainer.style.opacity = '1';
   } else {
     headerContainer.style.backgroundColor = 'rgba(51, 51, 51, 0)';
@@ -18,3 +18,11 @@ document.addEventListener('scroll', function () {
 document.addEventListener('DOMContentLoaded', function () {
   animateFromBottom();
 });
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
